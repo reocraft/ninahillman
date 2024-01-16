a7d533c3ceca3dbb1f053a1e420ed48316264ca8
reocraft <48170885+reocraft@users.noreply.github.com>
2024-01-16 18:49:52 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav')
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".navbar");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
